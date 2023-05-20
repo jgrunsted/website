b6f75d8b3eee8704e3cfbee333b0c94037f0e696
Jaydon Grunsted <jgrunsted@gmail.com>
2023-05-20 19:22:53 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
+const Hi = (props) => {
+  const {num} = props
+  return (
+    <div>jabooba {num}</div>
+  )
+}
 
 function App() {
+
+  const [number, setNumber] = useState(0);
+
+  const add = () => {
+    setNumber(number + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hi num = {number}/>
+      <div>Number = {number}</div>
+      <button onClick = {add}>Add</button>
     </div>
   );
 }
